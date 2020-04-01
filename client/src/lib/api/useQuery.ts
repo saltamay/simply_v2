22af2fc6c8602d0208fa4e5183877184ae12b8d2
Sal Tamay <sal.tamay@outlook.com>
2020-03-31 20:06:03 -0400

    feat: Implement useReducer to update state

--- client/src/lib/api/useQuery.ts
+++ client/src/lib/api/useQuery.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useEffect, useCallback, useReducer } from 'react';
 import { server } from './server';
 
 interface State<TData> {
@@ -11,26 +11,50 @@ interface QueryResults<TData> extends State<TData> {
   refetch: () => void;
 }
 
+type Action<TData> =
+  | { type: 'FETCH' }
+  | { type: 'FETCH_SUCCESS'; payload: TData }
+  | { type: 'FETCH_ERROR' };
+
+const reducer = <TData>() => (state: State<TData>, action: Action<TData>) => {
+  switch (action.type) {
+    case 'FETCH':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        data: action.payload,
+        loading: false,
+        error: false
+      };
+    case 'FETCH_ERROR':
+      return { ...state, loading: false, error: true };
+    default:
+      throw new Error();
+  }
+};
+
 export const useQuery = <TData = any>(query: string): QueryResults<TData> => {
-  const [state, setState] = useState<State<TData>>({
+  const fetchReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(fetchReducer, {
     data: null,
     loading: false,
     error: false
   });
 
   const fetch = useCallback(() => {
-    setState({ data: null, loading: true, error: false });
     const fetchApi = async () => {
       try {
+        dispatch({ type: 'FETCH' });
         const { data, errors } = await server.fetch<TData>({ query });
 
         if (errors && errors.length) {
           throw new Error(errors[0].message);
         }
 
-        setState({ data, loading: false, error: false });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
       } catch (err) {
-        setState({ data: null, loading: false, error: true });
+        dispatch({ type: 'FETCH_ERROR' });
         throw console.error(err);
       }
     };
