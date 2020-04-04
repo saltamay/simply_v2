3a26aa2c5657a4cc539f1b8b6aadf43cdbec2d58
Sal Tamay <sal.tamay@outlook.com>
2020-04-04 19:46:13 -0400

    feat: Add Google api

---
+++ server/src/lib/api/Google.ts
@@ -0,0 +1,31 @@
+import { google } from 'googleapis';
+
+const auth = new google.auth.OAuth2(
+  process.env.G_CLIENT_ID,
+  process.env.G_CLIENT_SECRET,
+  `${process.env.PUBLIC_URL}/login`
+);
+
+export const Google = {
+  authUrl: auth.generateAuthUrl({
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    access_type: 'online',
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+  }),
+  logIn: async (code: string) => {
+    // make a request to Google using a "code" argument to get a user's access token
+    const { tokens } = await auth.getToken(code);
+
+    auth.setCredentials(tokens);
+
+    const { data } = await google.people({ version: 'v1', auth }).people.get({
+      resourceName: 'people/me',
+      personFields: 'emailAddresses,names,photos',
+    });
+
+    return { user: data };
+  },
+};
