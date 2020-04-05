dfff4ddcecb3177096997a9a3ee0fa2d7cf57ec6
Sal Tamay <sal.tamay@outlook.com>
2020-04-05 19:37:45 -0400

    feat: Build out the UI for log in

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { Listings } from './sections/Listings';
+import { Listings, Login } from './sections';
 import * as serviceWorker from './serviceWorker';
 
 import './styles/index.css';
+import { Layout } from 'antd';
 
 const client = new ApolloClient({
-  uri: '/api/v1/graphql'
+  uri: '/api/v1/graphql',
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Listings title='Tiny Bungalow Listings' />
+    {/* <Listings title='Tiny Bungalow Listings' /> */}
+    <Layout id='app'>
+      <Login />
+    </Layout>
   </ApolloProvider>,
   document.getElementById('root')
 );
--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Card, Layout, Typography } from 'antd';
+
+// Image Assets
+import googleLogo from './assets/google_logo.jpg';
+
+const { Content } = Layout;
+const { Text, Title } = Typography;
 
 export const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <Content className='log-in'>
+      <Card className='log-in-card'>
+        <div className='log-in-card__intro'>
+          <Title level={3} className='log-in-card__intro-title'>
+            <span role='img' aria-label='wave'>
+              👋
+            </span>
+          </Title>
+          <Title level={3} className='log-in-card__intro-title'>
+            Log in to Tiny Bungalow!
+          </Title>
+          <Text>Sign in with Google to start your real estate search!</Text>
+        </div>
+        <button className='log-in-card__google-button'>
+          <img
+            src={googleLogo}
+            alt='Google Logo'
+            className='log-in-card__google-button-logo'
+          />
+          <span className='log-in-card__google-button-text'>
+            Sign in with Google
+          </span>
+        </button>
+        <Text type='secondary'>
+          Note: By signing in, you'll be redirected to the Google consent form
+          to sign in with your Google account.
+        </Text>
+      </Card>
+    </Content>
   );
 };
