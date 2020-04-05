c01e4526b545ade04f9592f5ed06cfbcfde4997f
Sal Tamay <sal.tamay@outlook.com>
2020-04-05 19:52:37 -0400

    feat: Add client side routes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { Listings, Login } from './sections';
+import { Home, Listing, Listings, Login, NotFound, User } from './sections';
 import * as serviceWorker from './serviceWorker';
 
 import './styles/index.css';
@@ -12,12 +13,26 @@ const client = new ApolloClient({
   uri: '/api/v1/graphql',
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Layout id='app'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/listing/:id' component={Listing} />
+          <Route exact path='/listings/:location?' component={Listings} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/user/:id' component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
+  );
+};
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    {/* <Listings title='Tiny Bungalow Listings' /> */}
-    <Layout id='app'>
-      <Login />
-    </Layout>
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 );
--- client/src/sections/index.ts
+++ client/src/sections/index.ts
@@ -1,2 +1,6 @@
+export * from './Home';
+export * from './Listing';
 export * from './Listings';
 export * from './Login';
+export * from './NotFound';
+export * from './User';
