e4fccf3dd72d9369b325fa531c3224a9010f8b0d
Sal Tamay <sal.tamay@outlook.com>
2020-04-05 19:13:47 -0400

    Login: Create GraphQL docs for queries and mutations

---
+++ client/src/sections/Login/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  );
+};
--- client/src/sections/index.ts
+++ client/src/sections/index.ts
@@ -1,1 +1,2 @@
 export * from './Listings';
+export * from './Login';
